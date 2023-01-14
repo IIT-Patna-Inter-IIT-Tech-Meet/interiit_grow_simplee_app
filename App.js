1c683a0c4ae62bc3ed2adccc463c2af5576076b1
Anurag Deo <anuragdeo03@gmail.com>
2023-01-14 14:55:35 +0530

    Added the Profile page

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Login } from './screens/Login';
+import ProfilePage from './screens/Profile';
 
 export default function App() {
   return (
     <PaperProvider>
-      <Login/>
+      {/* <Login/> */}
+      <ProfilePage/>
     </PaperProvider>
   );
 }
---
+++ screens/Profile.js
@@ -0,0 +1,282 @@
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'tailwind-react-native-classnames';
+
+const ProfilePage = () => {
+    const [editing, setEditing] = useState(false);
+    const [name, setName] = useState('John Doe');
+    const [email, setEmail] = useState('johndoe@example.com');
+    const [phone, setPhone] = useState('555-555-5555');
+    const [bloodGroup, setBloodGroup] = useState('A+');
+    const [noticePeriod, setNoticePeriod] = useState('30 days');
+    const [userName, setUsername] = useState('john_doe');
+    const [password, setPassword] = useState('john_doe');
+
+    const handleEdit = (field) => {
+        setEditing(true);
+    };
+
+    const handleUpdate = () => {
+        setEditing(false);
+        // Send data to server
+    };
+
+
+    return (
+        // <SafeAreaView className="flex-1">
+        <ScrollView className="flex flex-col p-4 bg-[#181920]">
+            <Text style={tw`text-3xl font-bold mb-4 text-center text-white`}>Profile</Text>
+            <View style={tw`flex items-center`}>
+                <Image
+                    style={tw`w-24 h-24 rounded-full my-2 border-4 border-black`}
+                    source={require('../assets/images/avatar.png')}
+                />
+                <Text style={tw`ml-4 text-lg text-gray-400`}>User ID: 123456</Text>
+            </View>
+            <View style={tw`mt-4`}>
+                <View className="flex-1 flex-row justify-center gap-3">
+                <Text style={tw`text-xl text-center font-semibold text-gray-400 my-2`}>Profile Info</Text>
+                <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('name')}>
+                    <Image
+                        className="ml-2 h-7 w-7"
+                        source={require('../assets/images/edit_icon.png')}
+                    />
+                </TouchableOpacity>
+                </View>
+                <View className="border-b border-gray-400" />
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Name</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={name}
+                            onChangeText={setName}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={name}
+                                editable={false}
+                                onChangeText={setName}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('name')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Email</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={email}
+                            onChangeText={setEmail}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={email}
+                                editable={false}
+                                onChangeText={setEmail}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('email')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Phone</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={phone}
+                            onChangeText={setPhone}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={phone}
+                                editable={false}
+                                onChangeText={setPhone}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('phone')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Blood Group</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={bloodGroup}
+                            onChangeText={setBloodGroup}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={bloodGroup}
+                                editable={false}
+                                onChangeText={setBloodGroup}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('bloodGroup')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Notice Period</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={noticePeriod}
+                            onChangeText={setNoticePeriod}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={noticePeriod}
+                                editable={false}
+                                onChangeText={setNoticePeriod}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('noticePeriod')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <Text style={tw`text-xl text-center font-semibold text-gray-400 my-2`}>Credentials</Text>
+                <View className="border-b border-gray-400" />
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Username</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            value={userName}
+                            onChangeText={setUsername}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                value={userName}
+                                editable={false}
+                                onChangeText={setUsername}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('name')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View style={tw`mb-2`}>
+                    <Text className="text-xl font-semibold text-gray-400 my-2">Password</Text>
+                    {editing ? (
+                        <TextInput
+                            className="border border-gray-400 text-lg rounded bg-[#252a34]"
+                            selectionColor="#9ca3af"
+                            textColor="#9ca3af"
+                            secureTextEntry={true}
+                            value={password}
+                            onChangeText={setPassword}
+                        />
+                    ) : (
+                        <View style={tw`flex flex-row items-center`}>
+                            <TextInput
+                                className="rounded bg-[#252a34] w-80 text-lg border border-solid border-gray-300"
+                                selectionColor="#9ca3af"
+                                textColor="#9ca3af"
+                                underlineColor="transparent"
+                                secureTextEntry={true}
+                                value={password}
+                                editable={false}
+                                onChangeText={setPassword}
+                            />
+                            <TouchableOpacity style={tw`h-7 w-7`} onPress={() => handleEdit('name')}>
+                                <Image
+                                    className="ml-2 h-7 w-7"
+                                    source={require('../assets/images/edit_icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+                <View className="my-2"></View>
+            </View>
+            {editing && (
+                <>
+                    <View className="flex items-center">
+                        <Button className="border rounded-xl w-11/12 h-12 bg-[#00ff57] mt-4 mb-4" onPress={handleUpdate}>
+                            <Text className="text-gray-900 text-lg">Update</Text>
+                        </Button>
+                    </View>
+                    <View style={tw`my-2`}>
+
+                    </View></>
+            )}
+        </ScrollView>
+        // </SafeAreaView>
+    );
+};
+
+export default ProfilePage;
+
