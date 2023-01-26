dc5fe61bfdbfef69c0fcfa766ca5cdc6214811e1
Anurag Deo <anuragdeo03@gmail.com>
2023-01-26 15:44:56 +0530

    Added map along with delivery points

--- screens/Map.js
+++ screens/Map.js
@@ -2,20 +2,12 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React, { useState, useEffect} from "react";
 // import  { MapView, Marker, Polyline, MapViewDirections } from "react-native-maps";
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, Polyline} from 'react-native-maps';
 import { StyleSheet } from "react-native";
 import Geolocation from "@react-native-community/geolocation";
 import haversine from "haversine";
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  // },
-  // map: {
-  //   flex: 1,
-  //   height: 400,
-  //   width: 400,
-  // },
     container: {
       position: 'absolute',
       top: 0,
@@ -41,37 +33,38 @@ const GOOGLE_MAPS_APIKEY = 'AIzaSyA8UHc-D4VOdkBY1Hi-SgWScoMrijBAgYg'
 
 const deliveries = [
   {
-  latitude: 37.78825,
-  longitude: -122.4324,
+  latitude: 25.56254,
+  longitude: 84.86152,
 },
-{
-  latitude: 37.819888,
-  longitude: -122.485841,
+  {
+  latitude: 25.51247,
+  longitude: 84.87626,
 },
-{
-  latitude: 37.766311,
-  longitude: -122.434468,
-}
+  {
+  latitude: 25.52354,
+  longitude: 84.87875,
+},
+
 ];
 
 const pickups = [
   {
-  latitude: 37.790074,
-  longitude: -122.419541,
-},
-{
-  latitude: 37.779608,
-  longitude: -122.401567,
-},
-{
-  latitude: 37.770735,
-  longitude: -122.465636,
-}
+    latitude: 25.56254,
+    longitude: 84.84521,
+  },
+    {
+    latitude: 25.51247,
+    longitude: 84.86210,
+  },
+    {
+    latitude: 25.52354,
+    longitude: 84.87415,
+  },
 ];
 
 export const Maps = () => {
-  console.log(deliveries)
-  console.log(pickups)
+  // console.log(deliveries)
+  // console.log(pickups)
   const [currentLocation, setCurrentLocation] = useState(null);
   const [route, setRoute] = useState(null);
   const [nearest, setNearest] = useState(null);
@@ -114,58 +107,51 @@ export const Maps = () => {
   };
   return (
     <View style={styles.container}>
-    <MapView
-    style={styles.map}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}/>
-  </View>
-    // <View style={styles.container}>
-    //   {console.log(currentLocation) && console.log(nearest)}
-    //   {currentLocation && nearest ? (
-    //   <MapView
-    //       style={styles.map}
-    //       initialRegion={{
-    //         latitude: currentLocation.latitude,
-    //         longitude: currentLocation.longitude,
-    //         latitudeDelta: 0.0922,
-    //         longitudeDelta: 0.0421,
-    //       }}
-    //     >
-    //       {deliveries.map((delivery) => (
-    //         <Marker
-    //           key={`delivery-${delivery.latitude}-${delivery.longitude}`}
-    //           coordinate={delivery}
-    //           onPress={() => handleOnPress(delivery)}
-    //         />
-    //       ))}
-    //       {pickups.map((pickup) => (
-    //         <Marker
-    //           key={`pickup-${pickup.latitude}-${pickup.longitude}`}
-    //           coordinate={pickup}
-    //         />
-    //       ))}
-    //       <Polyline
-    //         coordinates={route ? route.coordinates : []}
-    //         strokeWidth={4}
-    //         strokeColor="#000"
-    //       />
-    //       <Marker coordinate={currentLocation} />
-    //       <Marker coordinate={nearest} />
-    //       <MapViewDirections
-    //         origin={currentLocation}
-    //         destination={nearest}
-    //         apikey={GOOGLE_MAPS_APIKEY}
-    //         strokeWidth={4}
-    //         strokeColor="#000"
-    //         onReady={handleRouteReady}
-    //         onError={(error) => console.log(error)}
-    //       />
-    //     </MapView>
-    //   ) : null}
-    // </View>
+      {currentLocation && nearest ? (
+      <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          {/* {deliveries.map((delivery) =>{console.log(delivery);)})} */}
+
+          {deliveries.map((delivery) => (
+            <Marker
+              key={`delivery-${delivery.latitude}-${delivery.longitude}`}
+              coordinate={delivery}
+              onPress={() => handleOnPress(delivery)}
+            />
+          ))}
+          {pickups.map((pickup) => (
+            <Marker
+              key={`pickup-${pickup.latitude}-${pickup.longitude}`}
+              coordinate={pickup}
+              pinColor = {"green"}
+            />
+          ))}
+          <Polyline
+            coordinates={route ? route.coordinates : []}
+            strokeWidth={4}
+            strokeColor="#000"
+          />
+          {/* {console.log(currentLocation)} */}
+          <Marker coordinate={currentLocation} />
+          {/* <Marker coordinate={nearest} /> */}
+          {/* <MapViewDirections
+            origin={currentLocation}
+            destination={nearest}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={4}
+            strokeColor="#000"
+            onReady={handleRouteReady}
+            onError={(error) => console.log(error)}
+          /> */}
+        </MapView>
+      ) : null}
+    </View>
   );
 };
